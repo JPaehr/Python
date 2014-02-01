b5847bb69daa6a693688eb173e468c88523951ee
JPaehr <j.paehr@gmx.de>
2014-02-01 22:38:54 +0100

    sub hinzugefuegt, add verbesser, kommentare hinzugefuegt, div erweitert

--- Bruch/bruch.py
+++ Bruch/bruch.py
@@ -4,26 +4,35 @@ from __future__ import division
 class Bruch(object):
 
     def __init__(self, zaehler = 0, nenner = 1):
-        self.__zaehler = zaehler
-        self.__nenner = nenner
+
+        [self.__zaehler, self.__nenner] = self.__kuerzen(zaehler, nenner)
 
     def __add__(self, other):
-        if type(other) == Bruch:
-            zaehler = self.__zaehler*other.__nenner + self.__nenner*other.__zaehler
+        if isinstance(other, Bruch):
+
+            zaehler = self.__zaehler * other.__nenner + self.__nenner * other.__zaehler
             nenner = self.__nenner * other.__nenner
 
             bruch = self.__kuerzen(zaehler, nenner)
 
             return Bruch(bruch[0], bruch[1])
-        elif type(other) == int:
+
+        elif isinstance(other, (int, long)):
             zaehler = self.__zaehler + other * self.__nenner
             nenner = self.__nenner
             bruch = self.__kuerzen(zaehler, nenner)
 
             return Bruch(bruch[0], bruch[1])
 
+    def __sub__(self, other):
+        if isinstance(other, Bruch):
+            return self.__add__(Bruch(-other.__zaehler, other.__nenner))
+        elif isinstance(other, (int, long)):
+            return self.__add__(-other)
+
     def __mul__(self, other):
         assert isinstance(other, Bruch)
+
         zaehler = self.__zaehler * other.__zaehler
         nenner = self.__nenner * other.__nenner
         bruch = self.__kuerzen(zaehler, nenner)
@@ -33,26 +42,32 @@ class Bruch(object):
     def __truediv__(self, other):
         if isinstance(other, Bruch):
             return self.__mul__(Bruch(other.__nenner, other.__zaehler))
+        elif isinstance(other, (int, long)):
+            return self.__mul__(Bruch(1, other))
 
     def getDezimal(self):
         return self.__zaehler / self.__nenner
 
     def __kuerzen(self, zaehler, nenner):
+
+        #Vorzeichen bereinigen -> in Zaehler schieben
         vorzeichen = 1
         if (zaehler < 0 and nenner > 0) or zaehler > 0 and nenner < 0:
             vorzeichen = -1
 
         zaehler = abs(zaehler)
         nenner = abs(nenner)
-        print zaehler
-        print nenner
-        klein = min([zaehler, nenner])
-        ggk = 1
+
+        #kleineren Wert fuer ggt finden
+        klein = int(min([zaehler, nenner]))
+
+        #groessten gemeinsamen Teiler finden
+        ggt = 1
         for i in range(1, klein + 1):
             if zaehler % i == 0 and nenner % i == 0:
-                ggk = i
+                ggt = i
 
-        return [vorzeichen*zaehler/ggk, nenner/ggk]
+        return [vorzeichen*zaehler/ggt, nenner/ggt]
 
     def getZahler(self):
         return self.__zaehler
@@ -63,11 +78,11 @@ class Bruch(object):
     def check(self):
         print self.__kuerzen(self.__zaehler, self.__nenner)
 
-zahl1 = Bruch(-6, 2)
-zahl1.check()
-print zahl1.getZahler()
+zahl1 = Bruch(-100, 2)
 
-zahl2 = Bruch(1, 2)
+zahl2 = Bruch(-1, 2)
 
-zahl3 = zahl1 / zahl2
+zahl3 = zahl1 / 25
 print zahl3.getDezimal()
+print zahl3.getZahler()
+print zahl3.getNenner()
